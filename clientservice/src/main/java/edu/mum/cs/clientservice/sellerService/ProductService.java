00f9dd5bfc1acae8ba5658b69243dbbf3747a896
Wuletaw Adane <wule2000@gmail.com>
2019-08-13 17:03:16 -0500

    reviews and stuff

--- clientservice/src/main/java/edu/mum/cs/clientservice/sellerService/ProductService.java
+++ clientservice/src/main/java/edu/mum/cs/clientservice/sellerService/ProductService.java
@@ -5,6 +5,7 @@ package edu.mum.cs.clientservice.sellerService;
 import edu.mum.cs.clientservice.buyermodel.Cart;
 import edu.mum.cs.clientservice.sellermodel.Account;
 import edu.mum.cs.clientservice.sellermodel.Product;
+import edu.mum.cs.clientservice.sellermodel.Review;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.ParameterizedTypeReference;
@@ -48,10 +49,12 @@ public class ProductService {
         Product product1= restTemplate.postForObject(sellerUrl+"/newProduct", request, Product.class);
         return  product1;
     }
-//    public Product updateProduct(Product product) {
-//        HttpEntity<Product> request = new HttpEntity<>(product);
-//        ResponseEntity<Product> response = restTemplate.exchange(sellerUrl+"/updateProduct",HttpMethod.PUT, request, Product.class);
-//        return response.getBody();
-//    }
+    public void deleteProduct(Long id) {
+        restTemplate.delete(sellerUrl+"/deleteProduct/"+id );
+    }
+    public List<Review> getReviews(Long id) {
+        ResponseEntity<List<Review>> allReviews = restTemplate.exchange(sellerUrl + "/getReviews/"+id, HttpMethod.GET, null, new ParameterizedTypeReference<List<Review>>() { });
+        return allReviews.getBody();
+    }
 
 }
