9583f8e080f1f860307f28c079fda7eaa5a61fc6
IRADUKUNDA JADO <>
2019-08-13 15:43:40 -0500

    some few changes i dont remember

--- clientservice/src/main/java/edu/mum/cs/clientservice/controller/ProductController.java
+++ clientservice/src/main/java/edu/mum/cs/clientservice/controller/ProductController.java
@@ -1,7 +1,12 @@
 package edu.mum.cs.clientservice.controller;
 
+import edu.mum.cs.clientservice.adminmodel.User;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import javax.servlet.http.HttpSession;
 
 @Controller
 public class ProductController {
@@ -11,7 +16,13 @@ public class ProductController {
     }
 
     @GetMapping("/checkout")
-    public String checkout(){
+    public String checkout(HttpSession session, Model model, RedirectAttributes redirectAttributes){
+        if(session.getAttribute("user") ==  null){
+            redirectAttributes.addFlashAttribute("msg","Please sign or create account for checkount");
+            return  "redirect:/logon";
+        }
+        User user = (User) session.getAttribute("user");
+        model.addAttribute("user",user);
         return "checkout";
     }
 
