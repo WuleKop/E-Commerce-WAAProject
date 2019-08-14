d886752c27d1d6afc2f667452f7ce67c51ab4223
Nguezang Nsaha <nsahaarsene@yahoo.com>
2019-08-13 22:09:37 -0500

    Spring security not working correctly
    Co-Authored-By: Jean de Dieu Iradukunda <iradukundajado@users.noreply.github.com>

--- clientservice/src/main/java/edu/mum/cs/clientservice/adminService/controller/adminController.java
+++ clientservice/src/main/java/edu/mum/cs/clientservice/adminService/controller/adminController.java
@@ -3,31 +3,31 @@ package edu.mum.cs.clientservice.adminService.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
-@Controller
+//@Controller
 public class adminController {
-    @GetMapping("/admin/login")
-    public String adminLogin(){
-        return "admin/admin_login";
-    }
-    @GetMapping("/admin/mainpanel")
-    public String adminHome(){
-        return "admin/admin_mainpanel";
-    }
-    @GetMapping("/admin/mainpanel/post_ads")
-    public String adminads(){
-        return "admin/post_ads";
-    }
-    @GetMapping("/admin/mainpanel/manage_posts")
-    public String managePosts(){
-        return "admin/manage_posts";
-    }
-    @GetMapping("/admin/mainpanel/reviews")
-    public String manageReviews(){
-
-        return "admin/reviews";
-    }
-    @GetMapping("/admin/403")
-    public String error403() {
-        return "/admin/403";
-    }
+//    @GetMapping("/admin/login")
+//    public String adminLogin(){
+//        return "admin/admin_login";
+//    }
+//    @GetMapping("/admin/mainpanel")
+//    public String adminHome(){
+//        return "admin/admin_mainpanel";
+//    }
+//    @GetMapping("/admin/mainpanel/post_ads")
+//    public String adminads(){
+//        return "admin/post_ads";
+//    }
+//    @GetMapping("/admin/mainpanel/manage_posts")
+//    public String managePosts(){
+//        return "admin/manage_posts";
+//    }
+//    @GetMapping("/admin/mainpanel/reviews")
+//    public String manageReviews(){
+//
+//        return "admin/reviews";
+//    }
+//    @GetMapping("/admin/403")
+//    public String error403() {
+//        return "/admin/403";
+//    }
 }
