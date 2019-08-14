b14243d32682e162ec6b19b1bc0e318453f47e1c
Njeane <50376803+Njeane@users.noreply.github.com>
2019-08-13 21:08:41 -0500

    a

--- clientservice/src/main/java/edu/mum/cs/clientservice/service/ClientService.java
+++ clientservice/src/main/java/edu/mum/cs/clientservice/service/ClientService.java
@@ -9,6 +9,8 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
+import java.util.List;
+
 @Service
 public class ClientService {
 
