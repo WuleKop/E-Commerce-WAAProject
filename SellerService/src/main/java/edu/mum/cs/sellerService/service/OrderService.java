6883ea949663856e0f28b923eaf75007356d8b37
Wuletaw Adane <wule2000@gmail.com>
2019-08-13 19:39:02 -0500

    Latest Seller Service

--- SellerService/src/main/java/edu/mum/cs/sellerService/service/OrderService.java
+++ SellerService/src/main/java/edu/mum/cs/sellerService/service/OrderService.java
@@ -5,6 +5,7 @@ import edu.mum.cs.sellerService.model.Order;
 import java.util.List;
 
 public interface OrderService {
+    Order saveOrder(Order order);
     Order save(Order order, Long pId);
     void delete(Long id, Long pId);
     List<Order> getOrderByProductId(Long pId);
--- SellerService/src/main/java/edu/mum/cs/sellerService/service/ProductOrderImpl.java
+++ SellerService/src/main/java/edu/mum/cs/sellerService/service/ProductOrderImpl.java
@@ -13,29 +13,42 @@ import java.util.List;
 
 @Service
 @Transactional
-public class ProductOrderImpl implements IProductorderService{
+public class ProductOrderImpl implements IProductorderService {
 
     @Autowired
     private ProductOrderRepository productOrderRepository;
 
     @Override
     public ProductOrder newProductOrder(ProductOrder productOrder) {
-        return  productOrderRepository.save(productOrder);
+        return productOrderRepository.save(productOrder);
     }
 
     @Override
     public ProductOrder updateProductOrder(ProductOrder productOrder) {
-        return  productOrderRepository.save(productOrder);
+        return productOrderRepository.save(productOrder);
     }
 
     @Override
     public List<Order> productOrders(ProductOrder productOrder) {
         //for()
-        return  new ArrayList<>();
+        return new ArrayList<>();
     }
 
     @Override
     public List<Product> orderProducts(Order order) {
+        return null;
+    }
+
+    @Override
+    public String addProductOrder(List<ProductOrder> productOrders) {
+        try {
+            for (ProductOrder p : productOrders) {
+                productOrderRepository.save(p);
+            }
+            return "Saved Successfully";
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
         return null;
     }
 }
