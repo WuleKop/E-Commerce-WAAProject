a83e259523dfd0ffcf420cab089582d2a18c39d2
Njeane <50376803+Njeane@users.noreply.github.com>
2019-08-13 19:17:14 -0500

    a
    update product-details page

--- clientservice/src/main/java/edu/mum/cs/clientservice/controller/AccountController.java
+++ clientservice/src/main/java/edu/mum/cs/clientservice/controller/AccountController.java
@@ -12,7 +12,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
-import sun.security.provider.MD5;
+//import sun.security.provider.MD5;
 
 import javax.servlet.http.HttpSession;
 import java.util.Map;
